556adfa22e7e7c4092275b193b9771457384494f
dgyarmati <dpolya.w@gmail.com>
2020-06-26 09:43:07 +0200

    added binary search speed test

--- src/python/grokking_algorithms/binary_search/binary_search.py
+++ src/python/grokking_algorithms/binary_search/binary_search.py
@@ -14,3 +14,21 @@ def binary_search(values, n):
             lo = mid + 1
         else:
             return mid
+
+
+def binary_search_speed(values, n):
+    lo = 0
+    hi = len(values) - 1
+    counter = 0
+
+    while lo <= hi:
+        mid = math.floor((lo + hi) / 2)
+        guess = values[mid]
+        if guess > n:
+            hi = mid - 1
+            counter += 1
+        elif guess < n:
+            lo = mid + 1
+            counter += 1
+        else:
+            return mid, counter
--- src/python/grokking_algorithms/binary_search/test/binary_search_test.py
+++ src/python/grokking_algorithms/binary_search/test/binary_search_test.py
@@ -27,3 +27,12 @@ class BinarySearchTest(unittest.TestCase):
         numbers = [1, 2, 3, 8, 12, 24]
         idx = binary_search.binary_search(numbers, 1113)
         self.assertEqual(None, idx)
+
+    def test_binary_search_speed_is_log_n(self):
+        numbers = [1, 2, 3, 8, 12, 24, 32, 64]
+        idx, speed = binary_search.binary_search_speed(numbers, 64)
+        # binary search speed is O(log2N); for this list, the worst case is 3 because we have 8 elements, and log 2 8 = 3 (2^3 = 8)
+        best_case_speed = 1
+        worst_case_speed = 3
+        self.assertEqual(7, idx)
+        self.assertTrue(speed <= worst_case_speed and speed >= best_case_speed)
