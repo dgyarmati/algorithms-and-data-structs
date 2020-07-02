52846c9145585713adab7fd8f6059287e422a5d0
dgyarmati <dpolya.w@gmail.com>
2020-07-02 14:12:00 +0200

    added factorial example

--- src/python/grokking_algorithms/recursion/test/test_recursive_examples.py
+++ src/python/grokking_algorithms/recursion/test/test_recursive_examples.py
@@ -1,9 +1,16 @@
 import unittest
 from src.python.grokking_algorithms.recursion.find_key_in_boxes import find_box_with_key
+from src.python.grokking_algorithms.recursion.factorial import factorial
 
 
-class TestFindKeyInBoxes(unittest.TestCase):
+class TestRecursiveExamples(unittest.TestCase):
     def test_find_key_in_boxes_finds_key(self):
         box = [[], [], ["key"], []]
         box_with_key = find_box_with_key(box)
         self.assertEqual("key", box_with_key[0])
+
+    def test_factorial(self):
+        n = 5
+        expected = 120
+        actual = factorial(n)
+        self.assertEqual(expected, actual)
