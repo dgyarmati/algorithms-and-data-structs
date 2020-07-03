7e58016e4fba37444f773496d1e35fb8279db81d
dgyarmati <dpolya.w@gmail.com>
2020-07-03 15:07:55 +0200

    added recursive bubble sort and tests

--- src/python/common_sernse_guide_to_data_structures_and_algorithms/bubble_sort/bubble_sort.py
+++ src/python/common_sernse_guide_to_data_structures_and_algorithms/bubble_sort/bubble_sort.py
@@ -8,3 +8,15 @@ def bubble_sort(items):
             if j <= (len(items) - 1) and items[i] > items[j]:
                 items[i], items[j] = items[j], items[i]
                 ordered = False
+
+
+def recursive_bubble_sort(items, size):
+    if size <= 1:
+        return
+
+    for i in range(size):
+        j = i + 1
+        if j < size and items[i] > items[j]:
+            items[i], items[j] = items[j], items[i]
+
+    return recursive_bubble_sort(items, size - 1)
--- src/python/common_sernse_guide_to_data_structures_and_algorithms/bubble_sort/test/test_bubble_sort.py
+++ src/python/common_sernse_guide_to_data_structures_and_algorithms/bubble_sort/test/test_bubble_sort.py
@@ -1,5 +1,6 @@
 import unittest
 from src.python.common_sernse_guide_to_data_structures_and_algorithms.bubble_sort.bubble_sort import bubble_sort
+from src.python.common_sernse_guide_to_data_structures_and_algorithms.bubble_sort.bubble_sort import recursive_bubble_sort
 
 
 class TestBubbleSort(unittest.TestCase):
@@ -23,4 +24,26 @@ class TestBubbleSort(unittest.TestCase):
     def test_bubble_sort_sorts_empty_elements(self):
         items = []
         bubble_sort(items)
+        self.assertEqual([], items)
+
+    def test_recursive_bubble_sort_sorts_two_elements(self):
+        items = [8, -12]
+        expected = [-12, 8]
+        recursive_bubble_sort(items, len(items))
+        self.assertEqual(expected, items)
+
+    def test_recursive_bubble_sort_sorts_elements(self):
+        items = [3, 12, -80, 3, 8]
+        expected = [-80, 3, 3, 8, 12]
+        recursive_bubble_sort(items, len(items))
+        self.assertEqual(expected, items)
+
+    def test_recursive_bubble_sort_sorts_one_element(self):
+        items = [1]
+        recursive_bubble_sort(items, len(items))
+        self.assertEqual([1], items)
+
+    def test_recursive_bubble_sort_sorts_empty_elements(self):
+        items = []
+        recursive_bubble_sort(items, len(items))
         self.assertEqual([], items)
