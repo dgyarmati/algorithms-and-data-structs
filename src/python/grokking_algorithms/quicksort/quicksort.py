738665b2a78bc7f22babd8762fbc0bca9ab78d9a
dgyarmati <dpolya.w@gmail.com>
2020-07-03 12:38:53 +0200

    cleanup

--- src/python/grokking_algorithms/quicksort/quicksort.py
+++ src/python/grokking_algorithms/quicksort/quicksort.py
@@ -28,7 +28,7 @@ def partition(items, first_idx, last_idx):
         while lo_idx <= hi_idx and items[lo_idx] <= pivot_item:
             lo_idx = lo_idx + 1
 
-        while items[hi_idx] >= pivot_item and hi_idx >= lo_idx:
+        while hi_idx >= lo_idx and items[hi_idx] >= pivot_item:
             hi_idx = hi_idx - 1
 
         if hi_idx < lo_idx:
