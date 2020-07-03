96ed29e9bab50ab56dc9dcaa95be204ef21457f1
dgyarmati <dpolya.w@gmail.com>
2020-07-03 11:32:42 +0200

    added quicksort implementation and tests

--- src/python/grokking_algorithms/quicksort/quicksort.py
+++ src/python/grokking_algorithms/quicksort/quicksort.py
@@ -1,9 +1,40 @@
 # naive, inefficient solution
-def quicksort(items):
-    if len(items) < 2:
-        return items
-    else:
-        pivot = items[0]
-        less = [i for i in items[1:] if i < pivot]
-        greater = [i for i in items[1:] if i > pivot]
-        return quicksort(less) + [pivot] + quicksort(greater)
+# def quicksort(items):
+#     if len(items) < 2:
+#         return items
+#     else:
+#         pivot = items[0]
+#         less = [i for i in items[1:] if i < pivot]
+#         greater = [i for i in items[1:] if i > pivot]
+#         return quicksort(less) + [pivot] + quicksort(greater)
+
+
+def quicksort(items, first_idx, last_idx):
+    if first_idx < last_idx:
+        pivot_idx = partition(items, first_idx, last_idx)
+        quicksort(items, first_idx, pivot_idx - 1)
+        quicksort(items, pivot_idx + 1, last_idx)
+
+# TODO rewrite to use mid point as pivot
+def partition(items, first_idx, last_idx):
+    pivot_item = items[first_idx]
+
+    lo_idx = first_idx + 1
+    hi_idx = last_idx
+
+    while True:
+
+        while lo_idx <= hi_idx and items[lo_idx] <= pivot_item:
+            lo_idx = lo_idx + 1
+
+        while items[hi_idx] >= pivot_item and hi_idx >= lo_idx:
+            hi_idx = hi_idx - 1
+
+        if hi_idx < lo_idx:
+            break
+        else:
+            items[lo_idx], items[hi_idx] = items[hi_idx], items[lo_idx]
+
+    items[first_idx], items[hi_idx] = items[hi_idx], items[first_idx]
+
+    return hi_idx
--- src/python/grokking_algorithms/quicksort/test/quicksort_test.py
+++ src/python/grokking_algorithms/quicksort/test/quicksort_test.py
@@ -8,38 +8,50 @@ class QuicksortTest(unittest.TestCase):
     def test_quicksort_returns_empty_list_when_sorting_empty_list(self):
         items = []
         expected = []
-        actual = quicksort(items)
-        self.assertEqual(expected, actual)
+        quicksort(items, 0, len(items) - 1)
+        self.assertEqual(expected, items)
 
     def test_quicksort_returns_one_element_list_when_sorting_one_element_list(self):
         items = [1]
         expected = [1]
-        actual = quicksort(items)
-        self.assertEqual(expected, actual)
+        quicksort(items, 0, len(items) - 1)
+        self.assertEqual(expected, items)
 
     def test_quicksort_sorts_numbers_of_two_element_list(self):
         items = [4, 1]
         expected = [1, 4]
-        actual = quicksort(items)
-        self.assertEqual(expected, actual)
+        quicksort(items, 0, len(items) - 1)
+        self.assertEqual(expected, items)
 
     def test_quicksort_does_not_sort_sorted_list(self):
         items = [1, 4]
         expected = [1, 4]
-        actual = quicksort(items)
-        self.assertEqual(expected, actual)
+        quicksort(items, 0, len(items) - 1)
+        self.assertEqual(expected, items)
 
     def test_quicksort_sorts_numbers_of_three_element_list(self):
         items = [4, 1, -12]
         expected = [-12, 1, 4]
-        actual = quicksort(items)
-        self.assertEqual(expected, actual)
+        quicksort(items, 0, len(items) - 1)
+        self.assertEqual(expected, items)
+
+    def test_quicksort_sorts_numbers_of_four_element_list(self):
+        items = [4, 1, -1, -12]
+        expected = [-12, -1, 1, 4]
+        quicksort(items, 0, len(items) - 1)
+        self.assertEqual(expected, items)
+
+    def test_quicksort_sorts_numbers_list(self):
+        items = [29, 99, 27, 41, 66, 28, 44, 78, 87, 19, 31, 76, 58, 88, 83, 97, 12, 21, 44]
+        expected = [12, 19, 21, 27, 28, 29, 31, 41, 44, 44, 58, 66, 76, 78, 83, 87, 88, 97, 99]
+        quicksort(items, 0, len(items) - 1)
+        self.assertEqual(expected, items)
 
     def test_quicksort_sorts_numbers_of_arbitrary_element_list(self):
         items = generate_random_list()
-        actual = quicksort(items)
+        quicksort(items, 0, len(items) - 1)
         expected = sorted(items)
-        self.assertEqual(expected, actual)
+        self.assertEqual(expected, items)
 
 
 def generate_random_list():
