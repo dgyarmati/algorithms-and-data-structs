360f2ef37b8959e9c532af2482fbcb53aea22e8e
dgyarmati <dpolya.w@gmail.com>
2020-07-04 22:16:30 +0200

    added insertion sort solution

--- src/python/common_sernse_guide_to_data_structures_and_algorithms/insertion_sort/insertion_sort.py
+++ src/python/common_sernse_guide_to_data_structures_and_algorithms/insertion_sort/insertion_sort.py
@@ -1,2 +1,10 @@
-def insertion_sort(values):
-    pass
+def insertion_sort(items):
+    for i in range(1, len(items)):
+        position = i
+        temp_item = items[i]
+
+        while position > 0 and items[position - 1] > temp_item:
+            items[position] = items[position - 1]
+            position -= 1
+
+        items[position] = temp_item
--- src/python/common_sernse_guide_to_data_structures_and_algorithms/insertion_sort/test/test_insertion_sort.py
+++ src/python/common_sernse_guide_to_data_structures_and_algorithms/insertion_sort/test/test_insertion_sort.py
@@ -6,19 +6,23 @@ class TestInsertionSort(unittest.TestCase):
     def test_insertion_sort_sorts_empty_list(self):
         items = []
         expected = []
-        self.assertEqual(expected, insertion_sort(items))
+        insertion_sort(items)
+        self.assertEqual(expected, items)
 
     def test_insertion_sort_sorts_list_with_one_element(self):
         items = [1]
         expected = [1]
-        self.assertEqual(expected, insertion_sort(items))
+        insertion_sort(items)
+        self.assertEqual(expected, items)
 
     def test_insertion_sort_sorts_list_with_two_elements(self):
         items = [1, -2]
         expected = [-2, 1]
-        self.assertEqual(expected, insertion_sort(items))
+        insertion_sort(items)
+        self.assertEqual(expected, items)
 
     def test_insertion_sort_sorts_list_with_n_elements(self):
         items = [1, -2, 22, -234, 0, 12, 0]
         expected = [-234, -2, 0, 0, 1, 12, 22]
-        self.assertEqual(expected, insertion_sort(items))
+        insertion_sort(items)
+        self.assertEqual(expected, items)
