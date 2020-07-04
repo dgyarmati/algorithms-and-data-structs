81bddaa9c68b8ea34a491599ca584c590097e2a9
dgyarmati <dpolya.w@gmail.com>
2020-07-04 20:19:20 +0200

    added selection sort solution

--- src/python/grokking_algorithms/selection_sort/selection_sort.py
+++ src/python/grokking_algorithms/selection_sort/selection_sort.py
@@ -1,16 +1,28 @@
-def selection_sort(values):
-    sorted_values = []
-    for i in range(len(values)):
-        smallest_idx = find_smallest(values)
-        sorted_values.append(values.pop(smallest_idx))
-    return sorted_values
+# def selection_sort(values):
+#     sorted_values = []
+#     for i in range(len(values)):
+#         smallest_idx = find_smallest(values)
+#         sorted_values.append(values.pop(smallest_idx))
+#     return sorted_values
+#
+#
+# def find_smallest(values):
+#     smallest = values[0]
+#     smallest_idx = 0
+#     for i in range(1, len(values)):
+#         if values[i] < smallest:
+#             smallest = values[i]
+#             smallest_idx = i
+#     return smallest_idx
 
 
-def find_smallest(values):
-    smallest = values[0]
-    smallest_idx = 0
-    for i in range(1, len(values)):
-        if values[i] < smallest:
-            smallest = values[i]
-            smallest_idx = i
-    return smallest_idx
+#  O(n^2) time complexity
+def selection_sort(items):
+    for i in range(len(items)):
+        min_item_idx = i
+        for j in range(i + 1, len(items)):
+            if items[j] < items[min_item_idx]:
+                min_item_idx = j
+
+        if min_item_idx != i:
+            items[i], items[min_item_idx] = items[min_item_idx], items[i]
--- src/python/grokking_algorithms/selection_sort/test/selection_sort_test.py
+++ src/python/grokking_algorithms/selection_sort/test/selection_sort_test.py
@@ -4,6 +4,7 @@ from src.python.grokking_algorithms.selection_sort import selection_sort
 
 class SelectionSortTest(unittest.TestCase):
     def test_selection_sort_finds_value(self):
-        values = [156, 141, 35, 94, 88, 61, 111]
-        smallest_value = selection_sort.selection_sort(values)[0]
-        self.assertEqual(35, smallest_value)
+        items = [156, 141, 35, 94, 88, 61, 111]
+        expected = [35, 61, 88, 94, 111, 141, 156]
+        selection_sort(items)
+        self.assertEqual(expected, items)
